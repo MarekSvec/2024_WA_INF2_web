545dd162870f63fc564e45f00001ede9ef1b195a
MarekSvec <marek.svec@student.gyarab.cz>
2025-05-22 09:31:44 +0200

    final commit

--- content/models.py
+++ content/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
-    image = models.URLField(max_length=600)
+    image = models.CharField(max_length=600)
     order = models.IntegerField(default=0)
 
 
